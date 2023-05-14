dc167ff1a608ef46924eddb34dc6a791e84b26fa
nikolay <nik86olay@gmail.com>
2023-05-14 18:15:56 +0300

    testContainers commit

--- src/test/java/com/example/springboot/ApplicationTests.java
+++ src/test/java/com/example/springboot/ApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.springboot;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApplicationTests {
-
+    @Autowired
+    TestRestTemplate restTemplate;
+    private static final GenericContainer<?> myapp1 = new GenericContainer<>("prodapp:latest")
+            .withExposedPorts(8081);
+    // private static GenericContainer<?> myapp2 = new GenericContainer<>("devapp")
+    // .withExposedPorts(8080);
+    @BeforeEach
+    public void setUp() {
+        myapp1.start();
+        //myapp2.start();
+    }
     @Test
     void contextLoads() {
+        Integer myapp1Port = myapp1.getMappedPort(8081);
+        //Integer myapp2Port = myapp2.getMappedPort(8080);
+        System.out.println(myapp1Port);
+        // System.out.println(myapp2Port);
+        ResponseEntity<String> forEntity1 = restTemplate.getForEntity("http://localhost:" + myapp1Port, String.class);
+        //ResponseEntity<String> forEntity2 = restTemplate.getForEntity("http://localhost:" + myapp2Port, String.class);
+        System.out.println(forEntity1.getBody());
+        // System.out.println(forEntity2.getBody());
     }
-
 }
